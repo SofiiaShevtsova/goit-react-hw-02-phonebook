5be9248311ac6b97358f342ec51addc2df671e2f
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-12-13 19:35:00 +0200

    add start file

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'block',
+        color: '#010101',
+      }}
+    >
+    </div>
+  );
+};
